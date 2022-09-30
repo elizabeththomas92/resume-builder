7ced85ac108e82352a95f1aa4f65a148884fd11e
Elizabeth Thomas <elizabeththomas92@gmail.com>
2022-10-01 02:03:24 +0530

    improvement:added skills to resume

--- src/components/mainHeader.tsx
+++ src/components/mainHeader.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function MainHeader() {
   return (
-    <div className="border border-blue-400 border h-[200px] ">
+    <div className="border border-blue-400 h-[200px] flex justify-between p-3">
       <div className="p-4">
         <h1
           contentEditable="true"
@@ -22,6 +22,16 @@ function MainHeader() {
         >
           Short summary about yourself
         </h3>
+      </div>
+      <div className="relative">
+        <img
+          className="w-40 h-40 rounded-full border border-blue-100"
+          src="defaultProfile.jpeg"
+          alt="Rounded avatar"
+        />
+        <span className="top-2 left-28 absolute  w-3.5 h-3.5 bg-green-400 border-2 border-white dark:border-gray-800 rounded-full">
+          <input type="file" className="opacity-0" />
+        </span>
       </div>
     </div>
   );
--- src/components/resume.tsx
+++ src/components/resume.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import MainHeader from "./mainHeader";
+import Skills from "./skills";
 
 function Resume() {
   return (
     <div className="border border-black w-[55em] h-[100em] m-10">
       <MainHeader />
+      <Skills />
     </div>
   );
 }
