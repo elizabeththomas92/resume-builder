6c20f928c2b66bef80767cd7bcdbef4bd74e1ba0
Elizabeth Thomas <elizabeththomas92@gmail.com>
2022-10-04 00:18:43 +0530

    feature:experience section in progress

--- src/components/experience.tsx
+++ src/components/experience.tsx
@@ -7,21 +7,12 @@ function Experience() {
     <div className="flex flex-col p-3">
       <h1 className=" uppercase">Experience</h1>
       <div>
-        <button
-          onClick={() => setSkills([...skills, "New Skill"])}
-          className="text-xs font-semibold inline-block py-1 px-2 rounded text-blue-500 bg-blue-300 uppercase last:mr-0 mr-1"
-        >
-          + Add skill
-        </button>
-        {skills.map((skill, index) => (
-          <div
-            id={`skill-${index}`}
-            contentEditable={true}
-            className="text-xs font-semibold inline-block py-1 px-2 rounded text-blue-500 bg-blue-300 last:mr-0 mr-1"
-          >
-            {skill}
-          </div>
-        ))}
+        <h1 contentEditable="true" className="font-medium text-xl outline-none">
+          Title/Position
+        </h1>
+        <h1 contentEditable="true" className="font-medium  outline-none">
+          Company Name
+        </h1>
       </div>
     </div>
   );
