2bb7e102a9c4bbe176866f2a687185c5b3aa7af8
Elizabeth Thomas <elizabeththomas92@gmail.com>
2022-10-10 10:21:59 +0530

    feature:experience feature in progress, added from and to date

--- src/components/experience.tsx
+++ src/components/experience.tsx
@@ -13,6 +13,43 @@ function Experience() {
         <h1 contentEditable="true" className="font-medium  outline-none">
           Company Name
         </h1>
+        {/* Date */}
+        <div className="flex gap-1">
+          {/* from Date */}
+          <input
+            type="text"
+            placeholder="mm"
+            maxLength={2}
+            className="w-[44px]  border-b-2 border-dashed"
+          ></input>
+          <span>/</span>
+          <input
+            placeholder="yyyy"
+            type="text"
+            className="w-[44px]  border-b-2 border-dashed"
+            maxLength={4}
+          ></input>
+          <span className="mx-2">-</span>
+          {/* To Date */}
+          <input
+            type="text"
+            placeholder="mm"
+            maxLength={2}
+            className="w-[44px]  border-b-2 border-dashed"
+          ></input>
+          <span>/</span>
+          <input
+            placeholder="yyyy"
+            type="text"
+            className="w-[44px]  border-b-2 border-dashed"
+            maxLength={4}
+          ></input>
+          <label>
+            <input type="checkbox" name="html" value="html" /> Present
+          </label>
+        </div>
+        {/* Work Achievements */}
+        <div></div>
       </div>
     </div>
   );
